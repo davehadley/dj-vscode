88dfd291205ec6885c1de31c0a743299fa2f8539
David Hadley <davehadley@users.noreply.github.com>
2021-02-24 21:32:08 +0000

    Tests fail.

--- dj/src/test/suite/extension.test.ts
+++ dj/src/test/suite/extension.test.ts
@@ -4,9 +4,14 @@ import * as vscode from "vscode";
 import * as dj from "../../extension";
 
 suite("Extension Test Suite", () => {
-	vscode.window.showInformationMessage("Start all tests.");
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test("dj exports only activate and deactivate", () => {
-		assert.deepStrictEqual(Object.keys(dj), ["activate", "deactivate"]);
-	});
+  test("dj exports only activate and deactivate", () => {
+    assert.deepStrictEqual(Object.keys(dj), ["activate", "deactivate"]);
+  });
+
+  test("dj makes all commands available to VS Code", async () => {
+    let commands = await vscode.commands.getCommands(true);
+    assert(commands.includes("dj.capture"));
+  });
 });
