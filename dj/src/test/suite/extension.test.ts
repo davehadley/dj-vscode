262ed3b9c9b6d29c7f672e3498b53cac74e28469
David Hadley <davehadley@users.noreply.github.com>
2021-02-25 11:18:58 +0000

    Add directory.

--- dj/src/test/suite/extension.test.ts
+++ dj/src/test/suite/extension.test.ts
@@ -2,6 +2,7 @@ import * as assert from "assert";
 
 import * as vscode from "vscode";
 import * as dj from "../../extension";
+import * as fs from "fs";
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
@@ -13,5 +14,10 @@ suite("Extension Test Suite", () => {
   test("dj makes capture command available to VS Code", async () => {
     let commands = await vscode.commands.getCommands(true);
     assert(commands.includes("dj.capture"));
+  });
+
+  test("dj configuration has directory property", async () => {
+    let config = vscode.workspace.getConfiguration("dj");
+    assert("directory" in config);
   });
 });
