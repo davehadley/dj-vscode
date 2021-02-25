ba3a7cb4a62ec27800b12f61f246509ad9542811
David Hadley <davehadley@users.noreply.github.com>
2021-02-25 12:56:08 +0000

    Tests pass.

--- dj/src/commands.ts
+++ dj/src/commands.ts
@@ -2,10 +2,12 @@ import * as vscode from "vscode";
 import * as os from "os";
 import * as file from "./file";
 import * as directories from "./directories";
+import * as moment from "moment";
 
 export async function capture(): Promise<vscode.TextEditor> {
   console.log("dj capture");
   let path = await directories.inbox();
-  let uri = vscode.Uri.joinPath(path, "test.md");
+  let filename = moment().format("YYYY-MM-DD_hh-mm-ss");
+  let uri = vscode.Uri.joinPath(path, `${filename}.md`);
   return file.editFile(uri);
 }
--- dj/src/test/suite/commands.test.ts
+++ dj/src/test/suite/commands.test.ts
@@ -5,6 +5,7 @@ import * as commands from "../../commands";
 import * as directories from "../../directories";
 import * as fs from "fs";
 import * as path from "path";
+import * as moment from "moment";
 
 suite("Commands Test Suite", () => {
   vscode.window.showInformationMessage("Start commands tests.");
@@ -18,5 +19,14 @@ suite("Commands Test Suite", () => {
       path.dirname(uri.fsPath),
       await (await directories.inbox()).fsPath
     );
+  });
+
+  test("capture creates a new file with unique name", async () => {
+    let name = moment().format("YYYY-MM-DD_hh-mm-ss");
+    let editor = await commands.capture();
+    await editor.document.save();
+    let uri = editor.document.uri;
+    assert(fs.existsSync(uri.fsPath));
+    assert(uri.fsPath.includes(name));
   });
 });
